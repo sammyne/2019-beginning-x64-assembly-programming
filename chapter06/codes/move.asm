828bb977fa428849a2e9b7d37ce07a9b7089e458
sammyne <xiangminli@alumni.sjtu.edu.cn>
2022-08-22 01:48:18 +0000

    feature(chapter06/codes): dummy format

--- chapter06/codes/move.asm
+++ chapter06/codes/move.asm
@@ -1,9 +1,9 @@
 ; move.asm
 
 section .data
-  bNum db 123
-  wNum dw 12345
-  dNum dd 1234567890
+  bNum  db 123
+  wNum  dw 12345
+  dNum  dd 1234567890
   qNum1 dq 1234567890123456789
   qNum2 dq 123456
   qNum3 dq 3.14
@@ -15,14 +15,14 @@ section .text
 
 main:
   push rbp
-  mov rbp,rsp
+  mov rbp, rsp
   mov rax, -1             ; fill rax with 1s
   mov al, byte [bNum]     ; does NOT clear upper bits of rax
-  xor rax,rax             ; clear rax
+  xor rax, rax            ; clear rax
   mov al, byte [bNum]     ; now rax has the correct value
   mov rax, -1             ; fill rax with 1s
   mov ax, word [wNum]     ; does NOT clear upper bits of rax
-  xor rax,rax             ; clear rax
+  xor rax, rax            ; clear rax
   mov ax, word [wNum]     ; now rax has the correct value
   mov rax, -1             ; fill rax with 1s
   mov eax, dword [dNum]   ; does clear upper bits of rax
@@ -31,6 +31,6 @@ main:
   mov qword [qNum2], rax  ; one operand always a register
   mov rax, 123456         ; source operand an immediate value
   movq xmm0, [qNum3]      ; instruction for floating point
-  mov rsp,rbp
+  mov rsp, rbp
   pop rbp
   ret
