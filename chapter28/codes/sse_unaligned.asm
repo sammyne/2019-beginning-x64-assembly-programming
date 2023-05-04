1608a4962489977a0d0d1cc793cd9c2c6ac16e91
sammyne <xiangminli@alumni.sjtu.edu.cn>
2023-05-04 01:50:29 +0000

    feat(chapter28): dummy reformatting and add GAS counterpart

--- chapter28/codes/sse_unaligned.asm
+++ chapter28/codes/sse_unaligned.asm
@@ -17,14 +17,14 @@ section .data
             dq 2.2
   dpvector2 dq 3.3
             dq 4.4
-  fmt1      db "Single Precision Vector 1: %f, %f, %f, %f",10,0
-  fmt2      db "Single Precision Vector 2: %f, %f, %f, %f",10,0
+  fmt1      db "Single Precision Vector 1: %f, %f, %f, %f", 10, 0
+  fmt2      db "Single Precision Vector 2: %f, %f, %f, %f", 10, 0
   fmt3      db "Sum of Single Precision Vector 1 and Vector 2:"
-            db " %f, %f, %f, %f",10,0
-  fmt4      db "Double Precision Vector 1: %f, %f",10,0
-  fmt5      db "Double Precision Vector 2: %f, %f",10,0
+            db " %f, %f, %f, %f", 10, 0
+  fmt4      db "Double Precision Vector 1: %f, %f", 10, 0
+  fmt5      db "Double Precision Vector 2: %f, %f", 10, 0
   fmt6      db "Sum of Double Precision Vector 1 and Vector 2:"
-            db " %f, %f",10,0
+            db " %f, %f", 10, 0
 
 section .bss
   spvector_res resd 4
@@ -36,60 +36,60 @@ global main
 
 main:
   push rbp
-  mov rbp,rsp
+  mov rbp, rsp
   ; add 2 single precision floating point vectors
-  mov rsi,spvector1
-  mov rdi,fmt1
+  mov rsi, spvector1
+  mov rdi, fmt1
   call printspfp
-  mov rsi,spvector2
-  mov rdi,fmt2
+  mov rsi, spvector2
+  mov rdi, fmt2
   call printspfp
   movups xmm0, [spvector1]
   movups xmm1, [spvector2]
-  addps xmm0,xmm1
+  addps xmm0, xmm1
   movups [spvector_res], xmm0
-  mov rsi,spvector_res
-  mov rdi,fmt3
+  mov rsi, spvector_res
+  mov rdi, fmt3
   call printspfp
   ; add 2 double precision floating point vectors
-  mov rsi,dpvector1
-  mov rdi,fmt4
+  mov rsi, dpvector1
+  mov rdi, fmt4
   call printdpfp
-  mov rsi,dpvector2
-  mov rdi,fmt5
+  mov rsi, dpvector2
+  mov rdi, fmt5
   call printdpfp
   movupd xmm0, [dpvector1]
   movupd xmm1, [dpvector2]
-  addpd xmm0,xmm1
+  addpd xmm0, xmm1
   movupd [dpvector_res], xmm0
-  mov rsi,dpvector_res
-  mov rdi,fmt6
+  mov rsi, dpvector_res
+  mov rdi, fmt6
   call printdpfp
   leave
   ret
 
 printspfp:
   push rbp
-  mov rbp,rsp
+  mov rbp, rsp
   movss xmm0, [rsi]
-  cvtss2sd xmm0,xmm0
+  cvtss2sd xmm0, xmm0
   movss xmm1, [rsi+4]
-  cvtss2sd xmm1,xmm1
+  cvtss2sd xmm1, xmm1
   movss xmm2, [rsi+8]
-  cvtss2sd xmm2,xmm2
+  cvtss2sd xmm2, xmm2
   movss xmm3, [rsi+12]
-  cvtss2sd xmm3,xmm3
-  mov rax,4; four floats
+  cvtss2sd xmm3, xmm3
+  mov rax, 4          ; four floats
   call printf
   leave
   ret
 
 printdpfp:
   push rbp
-  mov rbp,rsp
+  mov rbp, rsp
   movsd xmm0, [rsi]
   movsd xmm1, [rsi+8]
-  mov rax,2; four floats
+  mov rax, 2          ; four floats
   call printf
   leave
   ret
